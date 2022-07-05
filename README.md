289be3588caffe822ec2d8b45ca71e0779a72c00
Dee Newcum <deenewcum@cpan.org>
2022-07-05 20:38:30 +0000

    minil new Perl-Tidy-Guarantee

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Perl::Tidy::Guarantee - It's new $module
+
+# SYNOPSIS
+
+    use Perl::Tidy::Guarantee;
+
+# DESCRIPTION
+
+Perl::Tidy::Guarantee is ...
+
+# LICENSE
+
+Copyright (C) Dee Newcum.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Dee Newcum <deenewcum@cpan.org>
---
+++ lib/Perl/Tidy/Guarantee.pm
@@ -0,0 +1,39 @@
+package Perl::Tidy::Guarantee;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Perl::Tidy::Guarantee - It's new $module
+
+=head1 SYNOPSIS
+
+    use Perl::Tidy::Guarantee;
+
+=head1 DESCRIPTION
+
+Perl::Tidy::Guarantee is ...
+
+=head1 LICENSE
+
+Copyright (C) Dee Newcum.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Dee Newcum E<lt>deenewcum@cpan.orgE<gt>
+
+=cut
+
