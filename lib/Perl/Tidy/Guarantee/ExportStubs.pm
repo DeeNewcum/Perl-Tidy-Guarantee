7913fd77427387d361a251034c162bf45b4f12f9
Dee Newcum <deenewcum@cpan.org>
2022-07-27 16:00:34 -0500

    where I left off

--- lib/Perl/Tidy/Guarantee/ExportStubs.pm
+++ lib/Perl/Tidy/Guarantee/ExportStubs.pm
@@ -39,6 +39,9 @@ our %export_stubs = _parse_export_stubs(<<'EOF');
 Moose
     extends with has before after around override augment super inner
 
+Catalyst
+    extends with has before after around override augment super inner
+
 Moose::Role
     extends with has before after around override augment super inner
 
@@ -51,60 +54,11 @@ Moo
 MooX
     extends with has before after around
 
-Dancer
-    after any before before_template cookie cookies config content_type dance dancer_version debug
-    del dirname info error engine false forward from_dumper from_json from_yaml from_xml get halt
-    header headers hook layout load load_app logger mime options param param_array params pass path
-    patch post prefix push_header put redirect render_with_layout request send_file send_error set
-    setting set_cookie session splat status start template to_dumper to_json to_yaml to_xml true
-    upload captures uri_for var vars warning
-
-Log::ger
-    log_trace log_debug log_info log_warn log_error log_fatal
-    log_is_trace log_is_debug log_is_info log_is_warn log_is_error log_is_fatal
-
-Coro
-    async async_pool cede schedule terminate current unblock_sub rouse_cb rouse_wait
-
-Net::EmptyPort
-    empty_port check_port
-
-File::Slurper
-    read_binary=OK read_text=OK read_lines=OK write_binary=OK write_text=OK read_dir=OK
-
-FormValidator::Lite::Constraint
-    rule file_rule alias delsp
-
-Types::Standard
-    LaxNum StrictNum Num ClassName RoleName Optional CycleTuple Dict Overload StrMatch OptList Tied
-    InstanceOf ConsumerOf HasMethods Enum Any Item Bool Undef Defined Value Str Int Ref CodeRef
-    RegexpRef GlobRef FileHandle ArrayRef HashRef ScalarRef Object Maybe Map Tuple
-
-Module::Signature
-    sign=OK verify=OK $SIGNATURE=OK $AUTHOR=OK $KeyServer=OK $Cipher=OK $Preamble=OK
-
-Carp::Assert
-    assert affirm should shouldnt DEBUG
-
-Readonly
-    Readonly Scalar=OK Array=OK Hash=OK Scalar1=OK Array1=OK Hash1=OK
-
-Test::More
-    ok use_ok require_ok is isnt like unlike is_deeply cmp_ok skip todo todo_skip pass fail eq_array
-    eq_hash eq_set $TODO plan done_testing can_ok isa_ok new_ok diag note explain subtest BAIL_OUT
-
-Date::Calc
-    Days_in_Year Days_in_Month Weeks_in_Year leap_year check_date check_time check_business_date
-    Day_of_Year Date_to_Days Day_of_Week Week_Number Week_of_Year Monday_of_Week
-    Nth_Weekday_of_Month_Year Standard_to_Business Business_to_Standard Delta_Days Delta_DHMS
-    Delta_YMD Delta_YMDHMS N_Delta_YMD N_Delta_YMDHMS Normalize_DHMS Add_Delta_Days Add_Delta_DHMS
-    Add_Delta_YM Add_Delta_YMD Add_Delta_YMDHMS Add_N_Delta_YMD Add_N_Delta_YMDHMS System_Clock
-    Today Now Today_and_Now This_Year Gmtime Localtime Mktime Timezone Date_to_Time Time_to_Date
-    Easter_Sunday Decode_Month Decode_Day_of_Week Decode_Language Decode_Date_EU Decode_Date_US
-    Fixed_Window Moving_Window Compress Uncompress check_compressed Compressed_to_Text Date_to_Text
-    Date_to_Text_Long English_Ordinal Calendar Month_to_Text Day_of_Week_to_Text
-    Day_of_Week_Abbreviation Language_to_Text Language Languages Decode_Date_EU2 Decode_Date_US2
-    Parse_Date ISO_LC ISO_UC
+inc::Module::Install
+    name all_from abstract abstract_from author author_from version version_from license
+    license_from perl_version perl_version_from recommends requires test_requires configure_requires
+    requires_external_bin install_script no_index installdirs install_as_core install_as_cpan
+    install_as_site install_as_vendor WriteAll
 
 # vars.pm exports the requested scalars/lists/hashes, and it should be allowed to do so
 vars
@@ -119,13 +73,22 @@ Fcntl
 Socket
     (don't stub)
 
+# sets several useful variables
+FindBin
+    (don't stub)
+
+# sets $opt_* as a side-effect
+Getopt::Std
+    (don't stub)
+
+Test::More
+    (don't stub)
+
 # ------------------------------------------------------------------------------
 EOF
 
 #use Data::Dumper;
-#print STDERR Dumper \%export_stubs;
-#print STDERR Dumper \%do_not_stub;
-#exit;
+#print STDERR Data::Dumper->Dump([\%export_stubs, \%do_not_stub], [qw(%export_stubs %do_not_stub)]); exit;
 
 
 # symbol suffixes:
